4432a47af993cb822dda9625a6f6d98508f3d067
Srikar KC <34884850+srikarkc@users.noreply.github.com>
2024-05-16 19:41:55 -0700

    added reading section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ReadingComponent } from './reading/reading.component';
 import { ResumeComponent } from './resume/resume.component';
 import { SkillsComponent } from './skills/skills.component';
 
@@ -24,5 +25,9 @@ export const routes: Routes = [{
 },{
     path: 'contact',
     component: ContactComponent
+},{
+    path: 'reading',
+    component: ReadingComponent
 }
+
 ];
---
+++ src/app/reading/reading.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+
+interface Book {
+    title: string;
+    author: string;
+    summary: string;
+    link: string;
+    category: string;  // Add a category field to the Book interface
+    readStatus: string; // 'Read' or 'To Read'
+}
+
+@Component({
+    selector: 'app-reading',
+    templateUrl: './reading.component.html',
+    styleUrls: ['./reading.component.css'],
+    standalone: true,
+    imports: [CommonModule, FormsModule]
+})
+export class ReadingComponent {
+    books: Book[] = [
+        {
+            title: 'Deep Learning',
+            author: 'Ian Goodfellow, Yoshua Bengio, Aaron Courville',
+            summary: 'An introduction to a broad range of topics in deep learning, covering mathematical and conceptual background.',
+            link: 'https://www.deeplearningbook.org',
+            category: 'Technical',
+            readStatus: 'Read'
+        },
+        {
+            title: 'The 7 Habits of Highly Effective People',
+            author: 'Stephen R. Covey',
+            summary: 'A holistic approach towards personal and professional effectiveness, which is a framework for personal development.',
+            link: 'https://www.example.com/7habits',
+            category: 'Self-Help',
+            readStatus: 'Read'
+        },
+        {
+            title: 'You Don’t Know JS',
+            author: 'Kyle Simpson',
+            summary: 'This book series dives deep into the core mechanisms of JavaScript, helping you to truly understand the language.',
+            link: 'https://www.example.com/ydkjs',
+            category: 'Technical',
+            readStatus: 'To Read'
+        },
+        {
+            title: '1984',
+            author: 'George Orwell',
+            summary: 'A dystopian novel by English novelist George Orwell, presenting a society under omnipresent government surveillance and public manipulation.',
+            link: 'https://www.example.com/1984',
+            category: 'Fiction',
+            readStatus: 'To Read'
+        }
+    ];
+
+    filteredBooks: Book[] = [];
+    selectedGenre: string = 'All';
+    selectedReadStatus: string = 'All';
+
+    constructor() {
+        this.filteredBooks = this.books;  // Initially display all books
+    }
+
+    filterBooks() {
+      this.filteredBooks = this.books.filter(book =>
+          (this.selectedGenre === 'All' || book.category === this.selectedGenre) &&
+          (this.selectedReadStatus === 'All' || book.readStatus === this.selectedReadStatus)
+      );
+  }
+
+}
